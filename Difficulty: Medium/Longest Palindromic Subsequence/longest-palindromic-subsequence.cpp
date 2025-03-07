4411b5d8e5ebec7cb4e6dc0b7c49b080f3b5d87a
Harpreet Kaur <79329465+officialHarpreetKaur@users.noreply.github.com>
2025-03-07 15:08:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int longestPalinSubseq(string &s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+    
+    // Base case: single letter palindromes
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 1;
+    }
+    
+    // Build the DP table
+    for (int length = 2; length <= n; length++) {
+        for (int i = 0; i <= n - length; i++) {
+            int j = i + length - 1;
+            if (s[i] == s[j]) {
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            } else {
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    
+    return dp[0][n - 1];
+        
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.longestPalinSubseq(s) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
